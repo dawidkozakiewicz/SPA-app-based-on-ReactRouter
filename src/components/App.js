23cef1b6219d3769a93539747af4332a28fa0065
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-03-27 20:17:28 +0100

    add route and css styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Link} from 'react-router-dom';
+import {BrowserRouter as Router, NavLink, Route} from 'react-router-dom';
 import './App.css';
  
+const Home = () => <h1>Strona startowa</h1>
+const News = () => <h1>Aktualności</h1>
+const Contact = () => <h1>kontakt do nas</h1>
 
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
+			<Router>
 			<div>
 			<header>
 				<nav>
-					{/* <Link to="/">Start</Link>
-					<Link to="/news">Aktualności</Link>
-					<Link to="/contact">Kontakt</Link> */}
-
-					 <li><Link to="/">Start</Link></li>
-					 <li><Link to="/news">Aktualności</Link></li>
-					 <li><Link to="/contact">Kontakt</Link></li>
+					{/* <NavLink to="/">Start</NavLink>
+					<NavLink to="/news">Aktualności</NavLink>
+					<NavLink to="/contact">Kontakt</NavLink> */}
+				<ul>	
+					 <li><NavLink to="/" exact activeClassName="home_selected" activeStyle={{backgroundColor: 'snow', letterSpacing: '6px', color: 'black'}}>Start</NavLink></li>
+					 <li><NavLink to="/news" exact activeClassName="news_selected" activeStyle={{backgroundColor: 'snow', letterSpacing: '6px', color: 'black'}}>Aktualności</NavLink></li>
+					 <li><NavLink to="/contact" exact activeClassName="contact_selected" activeStyle={{backgroundColor: 'snow', letterSpacing: '6px', color: 'black'}}>Kontakt</NavLink></li>
+				</ul>	 
 				</nav>
 				<section>
-					Strona -witaj!
+				<Route path="/" exact component={Home}/>
+				<Route path="/news" exact component={News}/>
+				<Route path="/contact" exact component={Contact}/>
 				</section>
 			</header>
 			</div>
-			</BrowserRouter>
+			</Router>
 		)
 	}
 }
